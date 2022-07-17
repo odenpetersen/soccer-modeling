93c60f399ed01c8c645507fe9dd4391c9cee5faf
Qthulhu42 <34537274+Qthulhu42@users.noreply.github.com>
2022-07-17 14:23:15 +1000

    Created data download script and associated .gitignore

---
+++ scripts/download.py
@@ -0,0 +1,41 @@
+from figshare.figshare import Figshare
+import os
+
+data =  {
+        "Events"        :   7770599,
+        "Matches"       :   7770422,
+        "Competitions"  :   7765316,
+        "Teams"         :   7765310,
+        "Players"       :   7765196,
+        "Referees"      :   8082665,
+        "Coaches"       :   8082650,
+        "Event names"   :   1174383,
+        "Tag names"     :   11743818
+        }
+
+def download():
+
+    os.system("rm -r ../data")
+    os.system("mkdir ../data")
+
+    print("Emptied data folder")
+
+    fs = Figshare()
+
+    for article_name in data.keys():
+
+        article_id = data[article_name]
+
+        number_of_files = len(fs.list_files(article_id))
+
+        fs.retrieve_files_from_article(article_id,"../data")
+
+        print(f'Downloaded {number_of_files} files for article "{article_name}" (#{article_id})')
+
+    os.system("mv ../data/figshare_*/* ../data/figshare/")
+    os.system("rmdir ../data/figshare_*")
+
+    print("Collated files")
+
+if __name__ == "__main__":
+    download()
